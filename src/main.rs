57b0ab91b4324f0112fb78b855e8a650ec6e45e2
Robert-George Crupa <robertcrupa@outlook.com>
2025-06-24 07:15:55 +0100

    Setup tun interface—now able to read ip packets straight from nic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Read;
+
+fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+
+    // Set up the network interface
+    let mut config = tun::Configuration::default();
+    config
+        .address((10, 0, 0, 6))
+        .netmask((255, 255, 255, 0))
+        .destination((10, 0, 0, 1))
+        .up();
+
+    #[cfg(target_os = "linux")]
+    config.platform_config(|config| {
+        // requiring root privilege to acquire complete functions
+        config.ensure_root_privileges(true);
+    });
+
+    let mut nic = tun::create(&config)?;
+    let mut buff = [0u8; 1504];
+    
+    // Read in from the tunnel
+    loop {
+        let read = nic.read(&mut buff)?;
+        println!("read {} bytes: {:?}", read, &buff[..read]);
+    }
 }
