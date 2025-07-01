d9e9df99c9254648d7f34593ea6e39b30564130d
Robert-George Crupa <robertcrupa@outlook.com>
2025-07-01 20:31:23 +0100

    Added ethernet parsing of TCP packets

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::io::Read;
-
 fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
 
     // Set up the network interface
@@ -8,6 +6,7 @@ fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
         .address((10, 0, 0, 6))
         .netmask((255, 255, 255, 0))
         .destination((10, 0, 0, 1))
+        .mtu(1504)
         .up();
 
     #[cfg(target_os = "linux")]
@@ -16,12 +15,23 @@ fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
         config.ensure_root_privileges(true);
     });
 
-    let mut nic = tun::create(&config)?;
+    let nic = tun::create(&config)?;
     let mut buff = [0u8; 1504];
     
     // Read in from the tunnel
     loop {
-        let read = nic.read(&mut buff)?;
-        println!("read {} bytes: {:?}", read, &buff[..read]);
+        let read = nic.recv(&mut buff)?;
+
+        if let Ok(packet) = etherparse::Ipv4HeaderSlice::from_slice(&buff[..read]) {
+            let source = packet.source_addr();
+            let dest = packet.destination_addr();
+
+            if let Ok(packet) = etherparse::TcpHeaderSlice::from_slice(&buff[packet.slice().len()..]) {
+                let src_port = packet.source_port();
+                let dest_port = packet.destination_port();
+
+                eprintln!("{} -> {} TCP {} {} -> {}", source, dest, packet.slice().len(), src_port, dest_port);
+            }
+        }
     }
 }
