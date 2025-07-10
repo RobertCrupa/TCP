7a3e9122e0e4b6fa859c45c2362073466627c42b
Robert-George Crupa <robertcrupa@outlook.com>
2025-07-10 19:07:37 +0100

    Implemented tcp state

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::collections::{hash_map, HashMap};
+use std::net::Ipv4Addr;
+
+use nix::libc::quad_t;
+mod tcp;
+
+#[derive(PartialEq, Eq, Hash)]
+struct Quad {
+    src: (Ipv4Addr, u16),
+    dest: (Ipv4Addr, u16)
+}
 fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
 
     // Set up the network interface
@@ -17,20 +28,30 @@ fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
 
     let nic = tun::create(&config)?;
     let mut buff = [0u8; 1504];
+
+    // TCP connection
+    let mut connections: HashMap<Quad, tcp::State> = Default::default();
     
     // Read in from the tunnel
     loop {
         let read = nic.recv(&mut buff)?;
+        
+        // Process the IPv4 packet
+        if let Ok(ip_hdr) = etherparse::Ipv4HeaderSlice::from_slice(&buff[..read]) {
+            let source = ip_hdr.source_addr();
+            let dest = ip_hdr.destination_addr();
 
-        if let Ok(packet) = etherparse::Ipv4HeaderSlice::from_slice(&buff[..read]) {
-            let source = packet.source_addr();
-            let dest = packet.destination_addr();
+            // Process TCP packets
+            if let Ok(tcp_hdr) = etherparse::TcpHeaderSlice::from_slice(&buff[ip_hdr.slice().len()..]) {
+                let src_port = tcp_hdr.source_port();
+                let dest_port = tcp_hdr.destination_port();
+                let header_size = ip_hdr.slice().len() + tcp_hdr.slice().len();
 
-            if let Ok(packet) = etherparse::TcpHeaderSlice::from_slice(&buff[packet.slice().len()..]) {
-                let src_port = packet.source_port();
-                let dest_port = packet.destination_port();
+                connections.entry(Quad {
+                    src: (source, src_port),
+                    dest: (dest, dest_port)
+                }).or_default().on_packet(ip_hdr, tcp_hdr, &buff[header_size..]);
 
-                eprintln!("{} -> {} TCP {} {} -> {}", source, dest, packet.slice().len(), src_port, dest_port);
             }
         }
     }
---
+++ src/tcp.rs
@@ -0,0 +1,25 @@
+pub struct State {}
+
+impl Default for State {
+    fn default() -> Self {
+        State {}
+    }
+}
+
+impl State {
+    pub fn on_packet<'a>(
+        &mut self,
+        ip_hdr: etherparse::Ipv4HeaderSlice<'a>,
+        tcp_hdr: etherparse::TcpHeaderSlice<'a>,
+        data: &'a [u8],
+    ) {
+        eprintln!(
+            "{}:{} -> {}:{} TCP size: {}",
+            ip_hdr.source_addr(),
+            tcp_hdr.source_port(),
+            ip_hdr.destination_addr(),
+            tcp_hdr.destination_port()
+            tcp_hdr.slice().len(),
+        );
+    }
+}
